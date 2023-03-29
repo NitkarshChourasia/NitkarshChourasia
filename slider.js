fdb31969ad6d54af5c11520f2eed1098a91312c6
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-03-30 01:02:23 +0530

    Adding fisher-tate algorithm for slider.js

--- slider.js
+++ slider.js
@@ -1,16 +1,31 @@
-var slideIndex = 0;
-showSlides();
+  var slideIndex = 0;
+  var slideOrder = shuffle(Array.from(Array(9).keys()));
 
-function showSlides() {
-  var i;
-  var slides = document.getElementById("slider").getElementsByTagName("img");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+  showSlides();
+
+  function showSlides() {
+    var i;
+    var slides = document.getElementById("slider").getElementsByTagName("img");
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+      slideOrder = shuffle(slideOrder);
+    }
+    slides[slideOrder[slideIndex - 1]].style.display = "block";
+    setTimeout(showSlides, 2000); // Change image every 2 seconds
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
+
+  function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+    while (0 !== currentIndex) {
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+    return array;
   }
-  slides[slideIndex - 1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-}
